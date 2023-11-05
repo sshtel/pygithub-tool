7e7568b6e29140a70d07d7edb919e43c6b25451c
Steve Song <sshtel@gmail.com>
2023-11-05 16:36:40 +0900

    update to fetch review comment and reviews of PRs

--- pygithub_test.py
+++ pygithub_test.py
@@ -47,11 +47,26 @@ print("-------------------------------")
 sinceDateTime = datetime.fromisoformat('2023-11-04T00:00:00')
 print(f"PR comments of {repository} since {sinceDateTime}")
 pr_comments = repository.get_pulls_comments(sort='created', since=sinceDateTime)
+
+### Reference: https://pygithub.readthedocs.io/en/latest/github_objects/PullRequestComment.html
 for pr_comment in pr_comments:
     print(pr_comment)
-
+    reactions = pr_comment.get_reactions()
+    ### Reference: https://pygithub.readthedocs.io/en/latest/github_objects/Reaction.html
+    for reaction in reactions:
+        print(reaction)
 print("-------------------------------")
 
+print(f"PR Review Comments of {repository} since {sinceDateTime}")
+get_pulls_review_comments = repository.get_pulls_review_comments(sort='created', since=sinceDateTime)
+
+### Reference: https://pygithub.readthedocs.io/en/latest/github_objects/PullRequestComment.html
+for pr_review_comment in get_pulls_review_comments:
+    print(pr_review_comment)
+    reactions = pr_review_comment.get_reactions()
+    for reaction in reactions:
+        print(reaction)
+print("-------------------------------")
 
 # get_pulls_comments(sort: Opt[str] = NotSet, direction: Opt[str] = NotSet, since: Opt[datetime] = NotSet) → PaginatedList[PullRequestComment]¶
 
@@ -71,17 +86,41 @@ for pull_request in pull_requests:
 
     # Iterate through commits
     for commit in commits:
-        print(f"commit: {commit}")
-        print(f"committer: {commit.commit.committer}")
-        print(f"committer.date: {commit.commit.committer.date}")
-        print(f"author: {commit.commit.author}")
-        print(f"author.date: {commit.commit.author.date}")
+        print(f"==> ==> commit: {commit}")
+        print(f"==> ==> committer: {commit.commit.committer}")
+        print(f"==> ==> committer.date: {commit.commit.committer.date}")
+        print(f"==> ==> author: {commit.commit.author}")
+        print(f"==> ==> author.date: {commit.commit.author.date}")
         lines_added_modified += commit.stats.additions + commit.stats.deletions
+
+
+    # Fetch comments for the pr
+    comments = pull_request.get_comments()
+    print(f"==> comments: {comments}")
+    for comment in comments:
+        print(comment)
+        reactions = comment.get_reactions()
+        for reaction in reactions:
+            print(reaction)
+
 
     # Fetch review comments for the pull request
     review_comments = pull_request.get_review_comments()
-    print(f"review_comments: {review_comments}")
+    print(f"==> review_comments: {review_comments}")
+    for review_comment in review_comments:
+        print(review_comment)
+        reactions = review_comment.get_reactions()
+        for reaction in reactions:
+            print(reaction)
+
     review_comment_count += review_comments.totalCount
+
+
+    # Fetch review 
+    reviews = pull_request.get_reviews()
+    print(f"==> reviews: {reviews}")
+    for review in reviews:
+        print(review)
 
 # Print the results
 print(f"Pull Requests: {pull_request_count}")
